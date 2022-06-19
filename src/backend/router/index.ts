64b0186db3fb155601be9111635d2f7474cf7042
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-06-19 23:30:27 +1000

    feat: cast vote api and mutation

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -2,17 +2,35 @@ import * as trpc from '@trpc/server'
 import { inferProcedureOutput } from '@trpc/server'
 import { PokemonClient } from 'pokenode-ts'
 import { z } from 'zod'
+import { prisma } from '$lib/prisma'
 
-export const appRouter = trpc.router().query('get-pokemon-by-id', {
-  input: z.object({
-    id: z.number(),
-  }),
-  async resolve({ input }) {
-    const client = new PokemonClient()
-    const pokemon = await client.getPokemonById(input.id)
-    return { name: pokemon.name, sprites: pokemon.sprites }
-  },
-})
+export const appRouter = trpc
+  .router()
+  .query('get-pokemon-by-id', {
+    input: z.object({
+      id: z.number(),
+    }),
+    async resolve({ input }) {
+      const client = new PokemonClient()
+      const pokemon = await client.getPokemonById(input.id)
+      return { name: pokemon.name, sprites: pokemon.sprites }
+    },
+  })
+  .mutation('cast-vote', {
+    input: z.object({
+      votedFor: z.number(),
+      votedAgainst: z.number(),
+    }),
+    async resolve({ input }) {
+      const voteInDb = await prisma.vote.create({
+        data: {
+          votedAgainstId: input.votedAgainst,
+          votedForId: input.votedFor,
+        },
+      })
+      return { success: true, vote: voteInDb }
+    },
+  })
 
 // export type definition of API
 export type AppRouter = typeof appRouter
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,8 +10,14 @@ export default function Home() {
   const firstPokemon = trpc.useQuery(['get-pokemon-by-id', { id: first }])
   const secondPokemon = trpc.useQuery(['get-pokemon-by-id', { id: second }])
 
+  const voteMutation = trpc.useMutation(['cast-vote'])
+
   const voteForRoundest = (selected: number) => () => {
-    setIds(getOptionsForVote())
+    if (selected === first) {
+      voteMutation.mutate({ votedFor: first, votedAgainst: second })
+    } else {
+      voteMutation.mutate({ votedFor: second, votedAgainst: first })
+    }
   }
 
   if (firstPokemon.isLoading || secondPokemon.isLoading)
