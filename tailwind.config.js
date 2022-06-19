91322d60cf8d40c518e82f440fb9ee8c7ca47c49
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-06-19 19:33:48 +1000

    feat: add google fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+        mono: ['"Space Mono"', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [],
 }
