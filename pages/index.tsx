960ed5da8346d23c3a697449f4fc35d6ad7a5b5d
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-06-19 18:12:45 +1000

    feat: basic homepage UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline md:text-5xl">Hello world!</h1>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-center text-2xl font-bold lg:text-4xl">
+        Which Pokemon is Roundest?
+      </h1>
+      <div className="p-8"></div>
+      <div className="flex max-w-2xl items-center justify-between space-x-10 rounded border p-8">
+        <div className="h-16 w-16 bg-red-200"></div>
+        <div className="font-mono">VS</div>
+        <div className="h-16 w-16 bg-red-200"></div>
+      </div>
     </div>
   )
 }
