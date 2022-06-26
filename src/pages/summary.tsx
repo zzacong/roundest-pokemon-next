114c77ff40bb9be68e7c7ca6dfa36aefeb9b84d3
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-06-26 19:13:35 +1000

    feat: page title

--- src/pages/summary.tsx
+++ src/pages/summary.tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps } from 'next'
 import { getPokemonInOrder, type PokemonQueryResult } from '$lib/utils'
 import { PokemonListing } from '$components'
 import { generateCountPercent } from '$components/PokemonListing'
+import Head from 'next/head'
 
 interface SummaryPageProps {
   pokemon: PokemonQueryResult
@@ -9,22 +10,28 @@ interface SummaryPageProps {
 
 export default function SummaryPage({ pokemon }: SummaryPageProps) {
   return (
-    <div className="mb-8 flex flex-col pt-16">
-      <h1 className="mb-8 text-center text-2xl font-bold lg:text-4xl">
-        Summary
-      </h1>
+    <>
+      <Head>
+        <title>Summary | Roundest Pokemon</title>
+      </Head>
 
-      <h3 className="mb-16 text-center text-xl">
-        Total pokemon:
-        <span className="font-mono font-bold"> {pokemon.length}</span>
-      </h3>
+      <div className="mb-8 flex flex-col pt-16">
+        <h1 className="mb-8 text-center text-2xl font-bold lg:text-4xl">
+          Summary
+        </h1>
 
-      <div className="mx-auto grid w-full max-w-2xl border">
-        {pokemon.map(p => (
-          <PokemonListing key={p.id} pokemon={p} />
-        ))}
+        <h3 className="mb-16 text-center text-xl">
+          Total pokemon:
+          <span className="font-mono font-bold"> {pokemon.length}</span>
+        </h3>
+
+        <div className="mx-auto grid w-full max-w-2xl border">
+          {pokemon.map(p => (
+            <PokemonListing key={p.id} pokemon={p} />
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
