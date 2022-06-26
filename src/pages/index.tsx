7ea65604b66cba2eede5b371bd96086d6d65e4b2
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-06-26 15:42:37 +1000

    feat: improve loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,29 +22,29 @@ export default function Home() {
     setIds(getOptionsForVote())
   }
 
-  if (firstPokemon.isLoading || secondPokemon.isLoading)
-    return <LoadingSpinner />
-
-  if (!firstPokemon.data || !secondPokemon.data)
-    return <LoadingSpinner message="no data to show" />
-
   return (
     <div className="flex grow flex-col items-center justify-center">
       <h1 className="mb-20 text-center text-2xl font-bold lg:text-4xl">
         Which Pokemon is Roundest?
       </h1>
 
-      <div className="flex max-w-2xl items-center justify-between space-x-10 rounded border py-8 px-16">
-        <PokemonVoting
-          pokemon={firstPokemon.data}
-          vote={voteForRoundest(first)}
-        />
-        <div className="font-mono text-2xl">VS</div>
-        <PokemonVoting
-          pokemon={secondPokemon.data}
-          vote={voteForRoundest(second)}
-        />
-      </div>
+      {firstPokemon.isLoading || secondPokemon.isLoading ? (
+        <LoadingSpinner />
+      ) : !firstPokemon.data || !secondPokemon.data ? (
+        <LoadingSpinner message="no data to show" />
+      ) : (
+        <div className="flex max-w-2xl items-center justify-between space-x-10 rounded border py-8 px-16">
+          <PokemonVoting
+            pokemon={firstPokemon.data}
+            vote={voteForRoundest(first)}
+          />
+          <div className="font-mono text-2xl">VS</div>
+          <PokemonVoting
+            pokemon={secondPokemon.data}
+            vote={voteForRoundest(second)}
+          />
+        </div>
+      )}
     </div>
   )
 }
